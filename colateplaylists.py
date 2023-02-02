18948dfe0541fe1d483ded75ee37679bd13724bd
FergusSteel <fergus.k.steel@gmail.com>
2023-02-02 19:15:02 +0000

    Tidied code + removed unnecessary files

--- colateplaylists.py
+++ colateplaylists.py
@@ -5,13 +5,7 @@ import spotipy.util as util
 from pprint import pprint
 import math
 import cgitb
-
-# Authorization FLow
-scope = "playlist-modify-public user-library-read playlist-read-collaborative playlist-read-private"
-user = ""#input("insert Spotify UserID") #Place UserID here ,
-token = util.prompt_for_user_token(user,scope,client_id="66cffc2706ab4a6fad63ece54c37e7ab",client_secret="",redirect_uri='http://localhost:8080')
-sp = spotipy.Spotify(auth=token)
-
+import sys
 
 # Function to create list of playlist ids
 def getPlaylistIDS(userID):
@@ -35,8 +29,11 @@ def get_playlist_trackIDS(playlist_id):
         while tracks:
             for track in tracks['items']:
                 curtrack = track["track"]
-                if curtrack["id"] != None and curtrack["track"]:
-                    trackIDS.append(curtrack["id"])
+                try:
+                    if curtrack["id"] != None and curtrack["track"]:
+                        trackIDS.append(curtrack["id"])
+                except:
+                    pass
             if tracks['next'] != None:
                 tracks = sp.next(tracks)
             else:
@@ -71,12 +68,11 @@ def get_saved_songs(user):
                 tracks = sp.next(tracks)
             else:
                 tracks = None
-    print(likedsongs)
     return likedsongs
-# Call
-def main(userID=user, removeDuplicates=True, addSavedSongs=True):
+
+def colate(sp, userID, pn, removeDuplicates=True, addSavedSongs=True):
     playlists = getPlaylistIDS(userID)
-    x = sp.user_playlist_create(userID, name="BIG PLAYLIST ITS SO BIG", description="its so big")
+    x = sp.user_playlist_create(userID, name=pn, description="")
     tracklist = get_tracklist(playlists)
     if addSavedSongs:
         tracklist.extend(get_saved_songs(userID))
@@ -88,4 +84,16 @@ def main(userID=user, removeDuplicates=True, addSavedSongs=True):
         sp.user_playlist_add_tracks(user, x["id"], tracklist[100*cent_tracks:(100*cent_tracks)+100])
     sp.user_playlist_add_tracks(user, x["id"], tracklist[math.floor(len(tracklist)/100)*100:])
 
-main()
+if __name__ == "__main__":
+    # Authorization FLow
+    scope = "playlist-modify-public user-library-read playlist-read-collaborative playlist-read-private"
+    user = input("insert Spotify UserID\n") #Place UserID here ,
+    print()
+    playlist_name = input("Insert Playlist Name:\n")
+    print()
+
+
+    token = util.prompt_for_user_token(user,scope,client_id="66cffc2706ab4a6fad63ece54c37e7ab",client_secret="b7764586c9084402b423c49f6ebe1c5c",redirect_uri='127.0.0.1:9090')
+    sp = spotipy.Spotify(auth=token)
+
+    colate(sp, user, pn)
